b1ae2ed9295b98c3e60c5816f5f9623e22d6da2d
Saksham2k3s <sakshamshrivastav58@gmail.com>
2024-09-19 18:37:28 +0530

    updated index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 
 //Configur CORS
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || '*',
     methods: 'GET,POST,PUT,DELETE', 
     credentials: true, 
   }));
